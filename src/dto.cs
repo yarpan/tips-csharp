ba414f585352c11b93673b4c3e5bf63578118666
Yaroslav Panchishyn <yarpan@gmail.com>
2024-10-19 21:50:58 +0300

    record

---
+++ src/dto.cs
@@ -0,0 +1,21 @@
+public class Product
+{
+    public string Name { get; init; }
+    public doublle Price { get; init; }
+    public int StockQuantity { get; init; }
+}
+
+
+
+
+public record Product(
+    string Name,
+    double Price,
+    int StockQuantity);
+
+
+
+// Choose 𝗿𝗲𝗰𝗼𝗿𝗱𝘀 when:
+// - You need immutable data types.
+// - You want built-in value equality.
+// - You’re creating simple DTOs without behavior.
