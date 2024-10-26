1bb1f5d3315993777f82bc0d861b76a8fdec1bbf
Yaroslav Panchishyn <yarpan@gmail.com>
2024-10-26 23:29:31 +0300

    update null

--- src/null.cs
+++ src/null.cs
@@ -1,7 +1,8 @@
 // Ways to guard against null arguments
 
 // 1a
-if (args is null){
+if (args is null)
+{
     throw new ArgumentNullException(nameof(args));
 }
 
@@ -13,15 +14,38 @@ value = arg ??
     throw new ArgumentNullException(nameof(arg));
 
 
+//========================================
 // Conditional expression
 // 2a
 if (customer is not null &&
     customer.Orders is not null)
-    {
-        customer.Orders.Ship();
-    }
-
+{
+    customer.Orders.Ship();
+}
 
 // 2b 
 customer?.Orders?.Ship();
 
+
+//========================================
+//Check for NOT null
+//3a 
+if (student != null)
+{
+    Console.Writeline('Student is NOT null');
+}
+
+//3b
+if (student is not null)
+{
+    Console.Writeline('Student is NOT null');
+}
+
+//3c 
+if (student is { })
+{
+    Console.Writeline('Student is NOT null');
+}
+
+
+//========================================
