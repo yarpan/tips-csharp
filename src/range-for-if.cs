6bb9dc45497afffac888ccf2042126204df7729d
Yaroslav Panchishyn <yarpan@gmail.com>
2024-10-29 12:24:26 +0200

    update

--- src/range-for-if.cs
+++ src/range-for-if.cs
@@ -1,12 +1,12 @@
 
-if (score >=60 && score <= 100)
+if (score >= 60 && score <= 100)
 {
     Console.WriteLine("Data in the expected range");
 }
 
 
 //Relational Pattern
-if (score is >=60 and <= 100)
+if (score is >= 60 and <= 100)
 {
     Console.WriteLine("Data in the expected range");
 }
