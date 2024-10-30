a29a24da8c5b622f83872c97efe13929c022118c
Yaroslav Panchishyn <yarpan@gmail.com>
2024-10-30 23:36:33 +0200

    add range

--- src/range.cs
+++ src/range.cs
@@ -11,3 +11,12 @@ if (score is >= 60 and <= 100)
     Console.WriteLine("Data in the expected range");
 }
 
+
+
+//Take with range
+
+List<int> numbers = GetNumbers();
+var result = numbers.Skip(6).Take(4);
+
+List<int> numbers = GetNumbers();
+var result = numbers.Take(6..10);
